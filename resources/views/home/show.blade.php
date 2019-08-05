d91112200b8cac56507bb58e3b972aa79588bd07
ryohichi <49134113+ryohichi@users.noreply.github.com>
2019-08-05 14:30:44 +0800

    Making login user's prifile page (#6)

--- resources/views/home/show.blade.php
+++ resources/views/home/show.blade.php
@@ -13,7 +13,7 @@
                     <img src="/images/{{ Auth::user()->avatar }}" alt="" class="mb-3 avatar">
                     <h2 class="mb-3">{{ Auth::user()->name }}</h2>
                     <h5 class="mb-3">{{ Auth::user()->email }}</h5>
-                    <a href="{{ route('home.show') }}" class="btn btn-primary mr-2">Your Page</a>
+                    
                     <a href="" class="btn btn-success">Update Profile</a>
                 </div>
                 <div class="user-follow p-2 my-3">
@@ -41,30 +41,14 @@
         <div class="col-8">
             <div class="post-list">
                 <div class="blog-list py-3 border bg-gray">
-                    <h2 class="text-center">Recent Activity</h2>
+                    <h2 class="text-center">Your Activity History</h2>
                     <div class="bg-white border border-top-secondary p-3 m-3">
-                        <div class="row">
-                            <div class="col-2">
-                                <img src="/images/default.png" alt="" class="avatar">
-                            </div>
-                            <div class="col-10">
-                                <a href=""><h4>Ryo</h4></a>
-                                <h4>Following Yuki</h4>
-                                <p>2 days ago</p>
-                            </div>
-                        </div>
+                        <h4>Following Yuki</h4>
+                        <p>2 days ago</p>
                     </div>
                     <div class="bg-white border border-top-secondary p-3 m-3">
-                        <div class="row">
-                            <div class="col-2">
-                                <img src="/images/default.png" alt="" class="avatar">
-                            </div>
-                            <div class="col-10">
-                                <a href=""><h4>Kye</h4></a>
-                                <h4>learned Fruite</h4>
-                                <p>3 days ago</p>
-                            </div>
-                        </div>
+                        <h4>learned New words</h4>
+                        <p>3 days ago</p>
                     </div>
                 </div>
             </div>
