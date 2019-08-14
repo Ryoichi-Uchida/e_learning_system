a90d14f55b9460a23d3204fc45919d56c5e2cfbf
Ryoichi-Uchida <ryo03_uchi_ec_sephi_lipse_loto@yahoo.co.jp>
2019-08-14 17:43:06 +0800

    Making delete function

--- app/Http/Controllers/Admin/QuestionController.php
+++ app/Http/Controllers/Admin/QuestionController.php
@@ -132,8 +132,10 @@ class QuestionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Question $question)
     {
-        //
+        $question->delete();
+
+        return redirect()->back();
     }
 }
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -24,7 +24,13 @@
                     <td class="d-flex justify-content-center border-0">
                         <a href="{{ route('question.create', ['category' => $category->id]) }}" class="btn btn-outline-primary mx-1">Add question</a>
                         <a href="" class="btn btn-outline-success mx-1">edit</a>
-                        <a href="" class="btn btn-outline-danger mx-1">delete</a>
+                        <form action="{{ route('category.destroy',['category' => $category->id]) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-outline-danger mx-1">
+                                <span>Delete</span>
+                            </button>
+                        </form>
                     </td>
                 </tr>
             @endforeach
