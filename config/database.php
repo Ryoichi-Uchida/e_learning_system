8662ad342d0cff5412806bea99fa916114e1a206
Ryoichi-Uchida <ryo03_uchi_ec_sephi_lipse_loto@yahoo.co.jp>
2019-08-27 14:55:02 +0800

    heroku set up

--- config/database.php
+++ config/database.php
@@ -1,5 +1,11 @@
 <?php
 
+    $url = parse_url(getenv('DATABASE_URL'));
+    $host = $url['host'] ?? null;
+    $username = $url['user'];
+    $password = $url['pass'];
+    $database = substr($url['path'], 1);
+
 return [
 
     /*
@@ -13,7 +19,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql_production'),
 
     /*
     |--------------------------------------------------------------------------
@@ -70,6 +76,19 @@ return [
             'sslmode' => 'prefer',
         ],
 
+        'pgsql_production' => [
+            'driver' => 'pgsql',
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
+ 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'host' => env('DB_HOST', 'localhost'),
